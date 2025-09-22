d17729aee0b7db38a0e026bbd37ee7c9dc884894
Valérie Song <valerie.song@ormex.io>
2025-09-22 22:36:14 +0200

    add admin routes

---
+++ src/app/features/admin/admin.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin';
+
+export const ADMIN_ROUTES: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+  },
+];
