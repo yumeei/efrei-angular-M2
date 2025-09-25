d5e6b61cef4072e9e8a6aa8c248614dbb398036e
Valérie Song <valerie.song@ormex.io>
2025-09-25 21:39:56 +0200

    add notification system

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
 import { TodoListComponent } from './features/todos/components/todo-list';
+import { NotificationsComponent } from './shared/components/notifications/notifications';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TodoListComponent],
+  imports: [RouterOutlet, HeaderComponent, TodoListComponent, NotificationsComponent],
   template: `
     <app-header></app-header>
     <main class="container mx-auto p-4">
       <router-outlet></router-outlet>
+      <app-notifications></app-notifications>
     </main>
   `,
   styles: [],
